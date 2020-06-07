4b17e07b9d889a1b46200995a872acee4590a7e9
Tina Xing <tinaxing14@gmail.com>
2020-06-07 13:58:31 -0400

    add product detail component

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,83 +1,96 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ProductDetail from "./ProductDetail";
-import ImageSlider from './utils/ImageSlider.jsx';
+import ImageSlider from "./utils/ImageSlider.jsx";
 
 //antd library imports
 import "antd/dist/antd.css";
-import { ShoppingCartOutlined, HeartOutlined } from '@ant-design/icons';
+import { ShoppingCartOutlined, HeartOutlined } from "@ant-design/icons";
 import { Col, Card, Row, Avatar } from "antd";
 const { Meta } = Card;
-
 
 const Product = () => {
   //react hooks
   const [products, setProducts] = useState([]);
   const [skip, setSkip] = useState(parseInt(0));
   const [dataSize, setDataSize] = useState(parseInt(8));
+  const [showProductDetail, setShowProductDetail] = useState(false);
+  const [currentProduct, setCurrentProduct] = useState(null);
 
   const loadProducts = (skip) => {
     axios
-    .get(`/api/products/${skip}`)
-    .then((response) => {
-      console.log(response.data);
-      setDataSize(response.data.length);
-      setProducts([...products, ...response.data]);
-    })
-    .catch((err) => {
-      console.log("failed to fetch data", err);
-    });
+      .get(`/api/products/${skip}`)
+      .then((response) => {
+        console.log(response.data);
+        setDataSize(response.data.length);
+        setProducts([...products, ...response.data]);
+      })
+      .catch((err) => {
+        console.log("failed to fetch data", err);
+      });
   };
 
   const loadMore = () => {
-    loadProducts(skip)
-    setSkip(skip + 8)
-  }
+    loadProducts(skip);
+    setSkip(skip + 8);
+  };
 
-  //same as componentdidmount
+  const handleProductDetail = (product) => {
+    setShowProductDetail(true);
+    setCurrentProduct(product);
+  };
+
+  //same as componentdidmount - gave an additional [] to prevent infinite loops
   useEffect(() => {
     loadProducts(skip);
-    setSkip(skip + 8)
+    setSkip(skip + 8);
   }, []);
 
   const renderCards = products.map((product, index) => {
     // dog or cat avatar for product card
     let avatar;
-    if(product.category ==='cat') {
-      avatar=<Avatar src="https://petfashion.s3.us-east-2.amazonaws.com/Screen Shot 2020-06-05 at 9.37.32 PM.png" />
-    } else if(product.category ==='dog') {
-      avatar=<Avatar src="https://petfashion.s3.us-east-2.amazonaws.com/Screen Shot 2020-06-05 at 9.37.36 PM.png" />
+    if (product.category === "cat") {
+      avatar = (
+        <Avatar src="https://petfashion.s3.us-east-2.amazonaws.com/Screen Shot 2020-06-05 at 9.37.32 PM.png" />
+      );
+    } else if (product.category === "dog") {
+      avatar = (
+        <Avatar src="https://petfashion.s3.us-east-2.amazonaws.com/Screen Shot 2020-06-05 at 9.37.36 PM.png" />
+      );
     }
     return (
-      <Col lg={6} md={8} xs={24}>
+      <Col lg={8} md={12} xs={24}>
         <Card
+          className='card'
+          onClick={() => {
+            handleProductDetail(product);
+          }}
+          key={index}
           hoverable={true}
-          style={{width:'300px'}}
-          cover={<ImageSlider images={product.images}/>}
-          actions={[
-            <ShoppingCartOutlined />,
-            <HeartOutlined />,
-          ]}
+          cover={<ImageSlider images={product.images.slice(0,3)} autoplay={false} classname='product_imageSlider' />}
+          actions={[<ShoppingCartOutlined />, <HeartOutlined />]}
         >
           <Meta
             avatar={avatar}
             title={product.name}
-            style={{height:'30px'}}
+            style={{ height: "30px" }}
             description={`$${product.price}`}
           />
         </Card>
       </Col>
     );
   });
+  
   return (
     <div className="product_container">
       <Row gutter={[15, 15]}>{renderCards}</Row>
-      {dataSize === 8 
-      ? <button onClick={loadMore}>Load More</button>
-      : null
-      }
+      <div>
+        {showProductDetail ? <ProductDetail product={currentProduct}/> : null}
+      </div>
+      <div>
+        {dataSize === 8 ? <button onClick={loadMore}>Load More</button> : null}
+      </div>
     </div>
-
   );
 };
 
--- client/src/components/ProductDetail.jsx
+++ client/src/components/ProductDetail.jsx
@@ -1,8 +1,39 @@
-import React from "react";
-const ProductDetail = () => (
-  <div>
-    
+import React, { useState, useEffect } from "react";
+import ImageSlider from './utils/ImageSlider.jsx';
+import axios from 'axios';
+
+const ProductDetail = ({product}) => {
+  //reverse image array to show the cloth front/back first
+  const [size, setSize] = useState('xs')
+  const images = product.images.reverse();
+  const addToCart = (id, size) => {
+    axios.post(`/api/products/addtocart?id=${id}&size=${size}`)
+      .then((response)=>{
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  return (<div className='productdetail_container'>
+    <div className='productdetail_image'>
+    <ImageSlider images={images} autoplay={true} classname='productdetail_imageSlider'/>
+    </div>
+    <div className='product_info'>
+      <div>header</div>
+      <div>{product.price}</div>
+      <div>{product.description}</div>
+      <div>{product.sizes.map((size, index) => {
+        return (
+          <div key={index} onClick={()=>{setSize(size)}}>{size}</div>
+        )
+      })}</div>
+      <div><button onClick={() => {addToCart(product.id, size)}}>Add to Cart</button></div>
+      <div>footer</div>
+    </div>
   </div>
 )
+}
 
 export default ProductDetail;
