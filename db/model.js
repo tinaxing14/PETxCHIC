4f44285b3a034ebc9933178fccd1115d87d05508
Tina Xing <tinaxing14@gmail.com>
2020-06-07 13:57:53 -0400

    add addcart model

--- db/model.js
+++ db/model.js
@@ -1,4 +1,4 @@
-const { Product } = require('./schema.js')
+const { Product, User } = require('./schema.js')
 
 
 module.exports = {
@@ -13,5 +13,56 @@ module.exports = {
           callback(null, results)
         }
       })
+  },
+  addCart: (id, size, callback) =>{
+    User.find({email: 'tinaxingtest@gmail.com'}, (err, result) => {
+      if(err) {
+        callback(err)
+      } else {
+        let duplicate = false;
+        result[0].cart.forEach(product => {
+          if(product.id === id && product.size === size) {
+            duplicate = true
+          }
+        });
+        if(duplicate) {
+          User.findOneAndUpdate(
+            {email: 'tinaxingtest@gmail.com', "cart.id": id, "cart.size" : size},
+            {$inc: {"cart.$.quantity":1}},
+            {new:true},
+            (err, result)=>{
+              if(err){
+                callback(err)
+              } else {
+                callback(null, result)
+              }
+            }
+          )
+        } else {
+          User.findOneAndUpdate(
+            {email: 'tinaxingtest@gmail.com'},
+            {
+              $push: {
+                cart: {
+                  id:id,
+                  size: size,
+                  quantity:1,
+                  date:Date.now()
+                }
+              }
+            },
+            { new: true},
+            (err, result) => {
+              if(err) {
+                callback(err)
+              } else {
+                callback(null, result)
+              }
+            }
+          )
+        }
+
+      }
+    })
   }
 }
