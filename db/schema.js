88334949c234efbf63b52334d9c696fdeb707211
Tina Xing <tinaxing14@gmail.com>
2020-06-07 13:54:36 -0400

    add user schema

--- db/schema.js
+++ db/schema.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const productsSchema = new Schema({
   id: {
     type: Number,
+    unique:1
   },
   name: String,
   category:String,
@@ -13,7 +14,28 @@ const productsSchema = new Schema({
   price: Number,
   sizes: Array
 });
+const Product = mongoose.model('Product', productsSchema, 'Products');
 
-const Product = mongoose.model('Product', productsSchema, 'Products')
+const userSchema = new Schema({
+  name: {
+    type:String,
+    maxlength:50
+  },
+  email: {
+    type:String,
+    trim:true,
+    unique:1,
+  },
+  password: {
+    type:String,
+    minlength:5
+  },
+  cart: {
+    type:Array,
+    default:[]
+  }
+});
 
-module.exports = { Product }
+const User = mongoose.model('User', userSchema, 'Users');
+
+module.exports = { Product, User }
