d56a5852ba5c59fcb4635d349e118716d3dda6ca
Tina Xing <tinaxing14@gmail.com>
2020-06-07 13:54:14 -0400

    add addtocart controller

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,4 @@
-const { getProducts } = require('../db/model.js')
+const { getProducts,addCart } = require('../db/model.js')
 module.exports = {
   getAllProducts: (req, res) => {
     const skip = parseInt(req.params.skip)
@@ -10,6 +10,18 @@ module.exports = {
         res.status(200).json(result);
       }
     })
+  },
+  addToCart: (req, res) => {
+    const {id, size } = req.query;
+    console.log(id, size)
+      addCart(id, size, (err, result) => {
+        if(err) {
+          console.log(err);
+          res.status(500).end()
+        } else {
+          res.status(201).json(result);
+        }
+      })
   }
 
 }
