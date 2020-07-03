a8e001bfe956eeada648a43436c03e70f5569b2d
Tina Xing <tinaxing14@gmail.com>
2020-07-03 13:27:12 -0400

    clean code

--- client/src/components/ProductDetail.jsx
+++ client/src/components/ProductDetail.jsx
@@ -8,7 +8,7 @@ import 'antd/dist/antd.css';
 const ProductDetail = ({product, setShowProductDetail, setCartNumber, cartNumber}) => {
   //reverse image array to show the cloth front/back first
   const [size, setSize] = useState('xs');
-  const images = product.images.reverse();
+  const images = product.images;
   const [spinner, setSpinner] = useState(false);
   const [sucess, setSucces] = useState('');
   const addToCart = (id, size, price, image, name) => {
@@ -29,29 +29,31 @@ const ProductDetail = ({product, setShowProductDetail, setCartNumber, cartNumber
   
   return (
     <div className='productdetail__black'>
-  <div className='productdetail_container'>
-    <div className='productdetail_image'>
-    <ImageSlider images={images} autoplay={true} classname='productdetail_imageSlider' dots={true}/>
+      <div className='productdetail_container'>
+        <div className='productdetail_image'>
+        <ImageSlider images={images} autoplay={true} classname='productdetail_imageSlider' dots={true}/>
+        </div>
+        <div className='product_info'>
+          <div className='productdetail__exitbtn' onClick={()=> { setShowProductDetail(false)}}>
+            <FullscreenExitOutlined style={{fontSize: '40px', color: 'black', cursor:"pointer"}} />
+          </div>
+          <div className="productdetail__title">{product.name}</div>
+          <div className="productdetail__price">{`$${product.price}`}</div>
+          <div className="productdetail__description">{product.description}</div>
+          <div className="productdetail__size__container">{product.sizes.map((size, index) => {
+            return (
+              <div className="productdetail__size" key={index} onClick={()=>{setSize(size)}}>{size}</div>
+            )
+          })}</div>
+          <div className='productdetail__size__selected'>Selected Size: {size}</div>
+          <div calssName='btn__container'>
+            <div><button className="productdetail__btn" onClick={() => {addToCart(product.id, size, product.price, product.images[0], product.name)}}>Add to Cart</button>
+          </div>
+          <div className='spinner__container'>
+            {spinner ? <div className="success__spinner"></div>: null}{sucess}</div>
+          </div>
+        </div>
     </div>
-    <div className='product_info'>
-      <div className='productdetail__exitbtn' onClick={()=> { setShowProductDetail(false)}}>
-        <FullscreenExitOutlined style={{fontSize: '40px', color: 'black', cursor:"pointer"}} />
-      </div>
-      <div className="productdetail__title">{product.name}</div>
-      <div className="productdetail__price">{`$${product.price}`}</div>
-      <div className="productdetail__description">{product.description}</div>
-      <div className="productdetail__size__container">{product.sizes.map((size, index) => {
-        return (
-          <div className="productdetail__size" key={index} onClick={()=>{setSize(size)}}>{size}</div>
-        )
-      })}</div>
-      <div className='productdetail__size__selected'>Selected Size: {size}</div>
-      <div calssName='btn__container'>
-      <div><button className="productdetail__btn" onClick={() => {addToCart(product.id, size, product.price, product.images[0], product.name)}}>Add to Cart</button></div>
-    <div className='spinner__container'>{spinner ? <div className="success__spinner"></div>: null}{sucess}</div>
-      </div>
-    </div>
-  </div>
   </div>
 )
 }
